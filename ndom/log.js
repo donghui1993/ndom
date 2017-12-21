476088b06f3aa1302a4d43ad0839d8dbfbed8a42
donghui <do_ng_hui@163.com>
2017-12-21 09:30:09 +0800

    error log

---
+++ ndom/log.js
@@ -0,0 +1,40 @@
+window.onerror = function errors(message,atfile,linenumber,columnnumber,error){
+    document.write(error.name+error.message+"@"+atfile +":"+ linenumber + ":"+columnnumber)
+}
+console.log = (function(oriLogFunc){
+    return function()
+    {
+    	var arr = [].slice.call(arguments);
+    	var str = arr.map(function(el){
+    		if(el+"" == '[object Object]'){
+    			return JSON.stringify(el)
+    		}
+    		return el;
+    	}).join(' ');
+    	try { throw new Error(); } catch (e) {
+        	var loc = e.stack.replace(/Error\n/).split(/\n/)[1].replace(/^\s+|\s+$/, "");
+            oriLogFunc.call(console,str+"\n"+loc);
+            document.body.innerText = "INFO : " +str +'\r\n'+ loc;
+        }
+    }
+})(console.log);
+console.error = (function(oriLogFunc){
+    return function()
+    {
+    	var arr = [].slice.call(arguments);
+    	var str = arr.map(function(el){
+    		if(el+"" == '[object Object]'){
+    			return JSON.stringify(el)
+    		}
+    		return el;
+    	}).join(' ');
+    	oriLogFunc.apply(console,arr);
+    	var strs = ""
+    	try { throw new Error(); } catch (e) {
+        	var stack = e.stack.split('\n').slice(2).join('\n');
+        	var loc= e.stack.replace(/Error\n/).split(/\n/)[1].replace(/^\s+|\s+$/, "");
+        	strs+= stack ;
+        }
+        document.body.innerText = "ERROR : " +str +'\r\n'+ strs;
+    }
+})(console.error);
